b17c26551a4f049ab178a7800279da3a9df0e2c3
jones-bass <jonesdev.tb@gmail.com>
2026-01-18 17:58:19 -0300

    Add Search

--- 02_Cars/cars/views.py
+++ 02_Cars/cars/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 from cars.models import Car
 
 def car_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
 
     return render (
         request,
