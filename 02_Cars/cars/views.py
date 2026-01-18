7895e2032ea56e1370308377fccab3209411e3fa
jones-bass <jonesdev.tb@gmail.com>
2026-01-18 12:35:37 -0300

    Iniciando estrutura view cars

--- 02_Cars/cars/views.py
+++ 02_Cars/cars/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from cars.models import Car
+
+def car_view(request):
+    cars = Car.objects.all()
+
+    return render (
+        request,
+        'cars.html',
+        {'cars': cars }
+    )  
+
+
+
+
+
+
+   
