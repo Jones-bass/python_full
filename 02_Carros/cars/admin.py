ba9890b0a468eba7eb660344fe25cc446e955668
jones-bass <jonesdev.tb@gmail.com>
2026-01-17 22:19:15 -0300

    Add modelo Admin-marcas

--- 02_Carros/cars/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from cars.models import Car
-
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'brand','factory_year', 'model_year', 'value')
-    search_fields = ('model',)
-
-admin.site.register(Car, CarAdmin)
