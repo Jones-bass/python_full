ad0404a57269568ebaf5ab46ae3126456c5ac810
jones-bass <jonesdev.tb@gmail.com>
2026-01-18 07:30:13 -0300

    Add Photo

--- 02_Cars/cars/models.py
+++ 02_Cars/cars/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
@@ -15,9 +14,9 @@ class Car (models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     plate = models.CharField(max_length=10, blank=True, null=True)
-    value = models.FloatField(blank=True, null=True)    
-    
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+        
     def __str__(self):
         return self.model
-    
     
