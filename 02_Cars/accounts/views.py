1b69eb08c320a7cb8617723ccddd933e81ad1f00
jones-bass <jonesdev.tb@gmail.com>
2026-01-20 20:46:02 -0300

    Add Rota Register

---
+++ 02_Cars/accounts/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('cars_list')
+    else:        
+        user_form = UserCreationForm()
+    return render(request, 'register.html', {'user_form':user_form})
+
