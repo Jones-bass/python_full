ac2f730230f6593876d2b1d39a37727491a1e696
jones-bass <jonesdev.tb@gmail.com>
2026-01-19 21:49:20 -0300

    Ajustando form e validacoes

--- 02_Cars/cars/forms.py
+++ 02_Cars/cars/forms.py
@@ -1,24 +1,19 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
 
-    def save(self):
-        car = Car(
-        model = self.cleaned_data['model'],
-        brand = self.cleaned_data['brand'],
-        factory_year = self.cleaned_data['factory_year'],
-        model_year = self.cleaned_data['model_year'],
-        plate = self.cleaned_data['plate'],
-        value = self.cleaned_data['value'],
-        photo=self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            raise forms.ValidationError('O valor do carro deve ser superior a R$ 20.000')
+        return value
+
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            raise forms.ValidationError('O ano de fabricação não pode ser inferior a 1975')
+        return factory_year
--- 02_Cars/cars/views.py
+++ 02_Cars/cars/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages  # Adiciona a importação para mensagens
 
 # Create your views here.
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
 
 def car_view(request):
     cars = Car.objects.all().order_by('model')
@@ -17,19 +18,17 @@ def car_view(request):
         {'cars': cars }
     )  
 
-
 def new_car_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
+        new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
+        else:
+            for field, errors in new_car_form.errors.items():
+                for error in errors:
+                    messages.error(request, f"Erro no campo {field}: {error}")
+        
     else:
-        new_car_form = CarForm( )
+        new_car_form = CarModelForm()
     return render(request, 'new_car.html', {'new_car_form': new_car_form })
-
-
-
-
-
-   
