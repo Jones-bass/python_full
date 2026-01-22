32b6a7d8cca59aba821e8853268364769cd7e177
jones-bass <jonesdev.tb@gmail.com>
2026-01-21 23:19:03 -0300

    Melhorando Create e ListViews

--- 02_Cars/cars/views.py
+++ 02_Cars/cars/views.py
@@ -1,34 +1,23 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages  # Adiciona a importação para mensagens
 
-# Create your views here.
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views.generic import ListView, CreateView
 
-def car_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    if search:
-        cars = cars.filter(model__icontains=search)
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars  
 
-    return render (
-        request,
-        'cars.html',
-        {'cars': cars }
-    )  
-
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        else:
-            for field, errors in new_car_form.errors.items():
-                for error in errors:
-                    messages.error(request, f"Erro no campo {field}: {error}")
-        
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form })
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'    
+    
