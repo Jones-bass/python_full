0a88cc9f8662a49212318883886a9a662d0de969
jones-bass <jonesdev.tb@gmail.com>
2026-02-19 23:41:51 -0300

    Add authenticacao no projeto

--- 03_Flix-API/movies/serializers.py
+++ 03_Flix-API/movies/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
+from django.db.models import Avg
 from movies.models import Movie
 
-
 class MovieSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        
+        return None
+    
