6c8edb68119cdab71226473d4c21b9f8b4d12c49
jones-bass <jonesdev.tb@gmail.com>
2026-02-22 21:20:42 -0300

    Add Projeto flix-app

---
+++ 03_flix-APP/app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+def main():
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Inicio')
+
+    if menu_option == 'Gêneros':
+        st.write('Gêneros')
+
+    if menu_option == 'Atores/Atrizes':
+        st.write('Atores/Atrizes')
+
+    if menu_option == 'Filmes':
+        st.write('Filmes')
+
+    if menu_option == 'Avaliações':
+        st.write('Avaliações')
+
+if __name__ == '__main__':
+    main()
