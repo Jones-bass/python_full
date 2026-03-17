fa341d9139c848ddd6cbde4cb5e88f1bcd3c4ee3
jones-bass <jonesdev.tb@gmail.com>
2026-03-17 20:07:39 -0300

    Add Repository - Service Actors

--- 03_flix-APP/actors/page.py
+++ 03_flix-APP/actors/page.py
@@ -1,32 +1,51 @@
-import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid
+import pandas as pd
+from datetime import datetime
+from st_aggrid import AgGrid, ExcelExportMode
+from actors.service import ActorService
 
-actors = [
-    {
-        'id': 1,
-        'name': 'Leonardo'
-    },
-    {
-        'id': 2,
-        'name': 'Souzinha'
-    },
-    {
-        'id': 3,
-        'name': 'Chirs Rock'
-    },
-]
-    
+
 def show_actors():
-    st.write('Linha de Atores/Atrizes:')
-    
-    AgGrid(
-        data=pd.DataFrame(actors),
-        reload_data=True,
-        key='actors_grid',
-    )
+    actor_service = ActorService()
+    actors = actor_service.get_actors()
 
-    st.title('Cadastrar novo(a) Ator/Atriz' )
-    name = st.text_input('Nome do(a) Ator/Atriz' )
+    if actors:
+        st.write('Lista de Atores/Atrizes:')
+        actors_df = pd.json_normalize(actors)
+        AgGrid(
+            data=actors_df,
+            reload_data=True,
+            columns_auto_size_mode=True,
+            enableSorting=True,
+            enableFilter=True,
+            enableColResize=True,
+            excel_export_mode=ExcelExportMode.MANUAL,
+            key='actors_grid',
+        )
+    else:
+        st.warning('Nenhum Ator/Atriz encontrado.')
+
+    st.title('Cadastrar Novo(a) Ator/Atriz')
+    name = st.text_input('Nome')
+    birthday = st.date_input(
+        label='Data de Nascimento',
+        value=datetime.today(),
+        min_value=datetime(1800, 1, 1).date(),
+        max_value=datetime.today(),
+        format='DD/MM/YYYY',
+    )
+    nationality_dropdown = ['BRAZIL', 'USA']
+    nationality = st.selectbox(
+        label='Nacionalidade',
+        options=nationality_dropdown,
+    )
     if st.button('Cadastrar'):
-        st.success (f'Ator/Atriz "{name}" cadastrado com sucesso!')
+        new_actor = actor_service.create_actor(
+            name=name,
+            birthday=birthday,
+            nationality=nationality,
+        )
+        if new_actor:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar o(a) Ator/Atriz. Verifique os campos')
