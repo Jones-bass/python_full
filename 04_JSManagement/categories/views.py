29cbefcb5d8a58044a2ab0f5fe52802c7e2ec858
jones-bass <jonesdev.tb@gmail.com>
2026-04-05 18:35:48 -0300

    CRUD Category

--- 04_JSManagement/categories/views.py
+++ 04_JSManagement/categories/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
 
-# Create your views here.
+
+class CategoryListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = models.Category
+    template_name = 'category_list.html'
+    context_object_name = 'categories'
+    paginate_by = 8
+    permission_required = 'categories.view_category'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+
+class CategoryCreateView(CreateView):
+    model = models.Category
+    template_name = 'category_create.html'
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('category_list')
+    permission_required = 'categories.add_category'
+
+
+class CategoryDetailView(DetailView):
+    model = models.Category
+    template_name = 'category_detail.html'
+    permission_required = 'categories.view_category'
+
+
+class CategoryUpdateView(UpdateView):
+    model = models.Category
+    template_name = 'category_update.html'
+    form_class = forms.CategoryForm
+    success_url = reverse_lazy('category_list')
+    permission_required = 'categories.change_category'
+
+
+class CategoryDeleteView(DeleteView):
+    model = models.Category
+    template_name = 'category_delete.html'
+    success_url = reverse_lazy('category_list')
+    permission_required = 'categories.delete_category'
