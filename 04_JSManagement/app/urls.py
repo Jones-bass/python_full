42ae093e2c3165ca021db4ebe5a837e49895ebbe
jones-bass <jonesdev.tb@gmail.com>
2026-04-06 21:06:56 -0300

    add CRUD supplier

--- 04_JSManagement/app/urls.py
+++ 04_JSManagement/app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    
+    path('', include('suppliers.urls')),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
 
--- 04_JSManagement/suppliers/views.py
+++ 04_JSManagement/suppliers/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from . import models, forms
 
-# Create your views here.
+
+class SupplierListView(ListView):
+    model = models.Supplier
+    template_name = 'supplier_list.html'
+    context_object_name = 'suppliers'
+    paginate_by = 8
+    permission_required = 'suppliers.view_supplier'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.GET.get('name')
+
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
+
+class SupplierCreateView(CreateView):
+    model = models.Supplier
+    template_name = 'supplier_create.html'
+    form_class = forms.SupplierForm
+    success_url = reverse_lazy('supplier_list')
+    permission_required = 'suppliers.add_supplier'
+
+
+class SupplierDetailView(DetailView):
+    model = models.Supplier
+    template_name = 'supplier_detail.html'
+    permission_required = 'suppliers.view_supplier'
+
+
+class SupplierUpdateView(UpdateView):
+    model = models.Supplier
+    template_name = 'supplier_update.html'
+    form_class = forms.SupplierForm
+    success_url = reverse_lazy('supplier_list')
+    permission_required = 'suppliers.change_supplier'
+
+
+class SupplierDeleteView(DeleteView):
+    model = models.Supplier
+    template_name = 'supplier_delete.html'
+    success_url = reverse_lazy('supplier_list')
+    permission_required = 'suppliers.delete_supplier'
+
+
