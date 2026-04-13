a0d28bd356352fb81f12a8bd616b43b7dd1b93ee
jones-bass <jonesdev.tb@gmail.com>
2026-04-13 00:16:33 -0300

    CRUD products

--- 04_JSManagement/app/urls.py
+++ 04_JSManagement/app/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', include('suppliers.urls')),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
-
+    path('', include('products.urls')),
+ 
     path('', include('inflows.urls')),
     path('', include('outflows.urls')),
 ]
